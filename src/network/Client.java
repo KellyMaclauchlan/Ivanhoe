82e03524b00cf20ca424a4fb3f703a45a12f72cb
kmbbeltran <katherinebeltran@cmail.carleton.ca>
2016-02-04 22:32:45 -0500

    networking test

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,10 +1,62 @@
 package network;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.net.Socket;
 
-	public boolean connectToServer(String ip, int port) {
+import config.Config;
+import org.apache.log4j.Logger;
 
-		return false;
+public class Client implements Runnable {
+	public int ID = 0;
+	public Socket socket = null;
+	public Thread thread = null;
+	public ClientThread client = null;
+	public BufferedReader console = null;
+	public BufferedReader inStream = null;
+	public BufferedWriter outStream = null;
+	String testing;
+	public Logger log = Logger.getLogger("Client");
+
+	public int getID(){
+		return this.ID;
+	}
+	
+	public Client(){
+		connectToServer(Config.DEFAULT_HOST, Config.DEFAULT_PORT);
+	}
+	
+	public boolean connectToServer(String serverIP, int serverPort) {
+		System.out.println(ID + ":Establishing connection. Please wait... ");
+		//log.info(ID + ":Establishing connection. Please wait... ");
+		boolean connected = false;
+		
+		try{
+			this.socket = new Socket(serverIP, serverPort);
+			this.ID = socket.getLocalPort(); 
+			
+			//log.info(ID + ": Connected to server: " + socket.getInetAddress());
+	    	//log.info(ID + ": Connected to portid: " + socket.getLocalPort());
+	    	
+	    	this.start();
+	    	//log.info("Client has started");
+	    	connected = true;
+		}catch(IOException e){
+			connected = false;
+			//log.error(e);
+		}
+		return connected;
+	}
+	private void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/network/Server.java
+++ src/network/Server.java
@@ -1,13 +1,45 @@
 package network;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashMap;
 
+import org.apache.log4j.Logger;
+
+import config.Config;
+
+public class Server {
+	public Thread thread = null;
+	public ServerSocket server = null;
+	public HashMap<Integer, ServerThread> clients;
+	public Logger log = Logger.getLogger("Server");
+
+	public Server(){
+		runServer(Config.DEFAULT_PORT);
+	}
+	
 	public void runServer(int port) {
+		try{
+			System.out.println("Binding to port " + port + ", please wait...");
+			log.info("Binding to port " + port + ", please wait...");
+			clients = new HashMap<Integer, ServerThread>();
+			server = new ServerSocket(port);
+			server.setReuseAddress(true);
+			start();
+		}catch(IOException e){
+			log.error(e);
+		}
+	}
+	private void start() {
 		
 	}
 
 	public void shutdown() {
-		
+		try {
+			server.close();
+		} catch (IOException e) {
+			log.error(e);
+		}
 	}
 
 }
