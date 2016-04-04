2ef2106d76718b0fe07e50f7f856ae4cfd5256c9
brittanyveinot <brittany.veinot@carleton.ca>
2016-04-04 12:28:03 -0400

    merge and test prep

--- src/junittests/AllTests.java
+++ src/junittests/AllTests.java
@@ -6,7 +6,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
-	//TestClient.class, 
 	TestGameStart2Player.class, 
 	TestGameEngine.class, 
 	TestScenarios.class, 
